17987ead1a59b4cd39837b8809931e2a00ff9ef0
Shafayet Jamil <shafayetemon88@gmail.com>
2023-03-09 11:27:21 +0600

    Products and Carts are connected

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -7,4 +7,12 @@ class Product < ApplicationRecord
         with: %r{\.(gif|jpg|png)\z}i,
         message: 'must be a URL for GIF, JPG or PNG image.'
     }
+
+    private
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                errors.add(:base, 'Line Items present')
+                throw :abort
+            end            
+        end
 end
